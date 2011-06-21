1f00d2d4d7f30f8e2457510eecbf1580fa8b2bac
Robert McBride <robmcbride@gmail.com>
2011-06-21 11:51:51 -0700

    new rails app, just added haml in gemfile

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1.0.rc'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'haml-rails'
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aboveall::Application.config.secret_token = '0fc526db680193e6fdee7a5a67a2486b70ffe32fcc190a2e9cb41406a1d5181991b5029049340a682f0fec94d61b69fc87f115a491b5b3a577132f8128a6e4dc'

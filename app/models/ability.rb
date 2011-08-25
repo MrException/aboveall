1735182a42523cd81aa7ff6d1c9267f48b9dd765
Robert McBride <robmcbride@gmail.com>
2011-08-25 11:50:19 -0700

    tweaking roles so users cant get to admin views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,14 @@ class Ability
 
   def initialize(user)
 
-    user ||= User.new # guest user (not logged in)
+    # guest user (not logged in)
+    # will have role of 'unauthorized'
+    user ||= User.new
 
     if user.admin?
       can :manage, :all
     elsif user.authorized?
-      can :read, :all
+      can :read, Product
     end
 
     # The first argument to `can` is the action you are giving the user permission to do.

279027797da595a0cdea97492818029cc92d197f
Robert McBride <robmcbride@gmail.com>
2011-09-29 23:04:59 -0700

    modifying seeds file so it uses factory_girl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'factory_girl'
+Dir[Rails.root.join("spec/factories.rb")].each {|f| require f}
 
 puts 'DELETING USERS AND ROLES'
 User.delete_all
 Role.delete_all
 
 puts 'CREATING ROLES'
-admin_role = Role.create! ({ :name => 'admin'})
-authorized_role = Role.create! ({ :name => 'authorized'})
-unauthorized_role = Role.create! ({ :name => 'unauthorized'})
+FactoryGirl.create :unauthorized_role
+FactoryGirl.create :authorized_role
+FactoryGirl.create :admin_role
 puts "Created three roles.\n"
 
+puts 'SETTING UP DEFAULT ADMIN USER'
+user = FactoryGirl.create :admin
+puts "Created admin user.\n"
+
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! ({ :first_name => 'Bobby',
-  last_name: 'McGee',
-  email: 'user@test.com',
-  password: 'please',
-  password_confirmation: 'please',
-  road: "213 Here Rd",
-  city: "Kelowna",
-  province: "BC",
-  city: "Kelowna",
-  postal_code: "a1a1a1",
-  gender: "M",
-  dob: Time.now
-})
-r = Role.where(:name => 'authorized').first
-user.role = r
-user.save
+user = FactoryGirl.create :user
 puts "Created new user.\n"
 
-puts 'SETTING UP DEFAULT ADMIN USER'
-user = User.create! ({ :first_name => 'Bobby',
-  last_name: 'McGee',
-  email: 'admin@test.com',
-  password: 'please',
-  password_confirmation: 'please',
-  road: "213 Here Rd",
-  city: "Kelowna",
-  province: "BC",
-  city: "Kelowna",
-  postal_code: "a1a1a1",
-  gender: "M",
-  dob: Time.now
-})
-r = Role.where(:name => 'admin').first
-user.role = r
-user.save
-puts "Created admin user.\n"
+puts 'SETTING UP DUMMY USER LOGIN'
+user = FactoryGirl.create :dummy_user
+puts "Created dummy user.\n"

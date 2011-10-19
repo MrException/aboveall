11eb4427ed2cc7e51e224699ad0d2eaa2a92e1b4
Robert McBride <robmcbride@gmail.com>
2011-10-19 16:20:53 -0700

    A couple of helpers added to appcontroller to get the cart and determine if it's to full.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
     flash[:error] = exception.message
     redirect_to root_url
   end
+
+  def current_cart
+    current_user.cart
+  end
+
+  def cart_over_limit?
+    current_cart.total_weight > current_user.possession_limit
+  end
 end

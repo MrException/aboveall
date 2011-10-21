293a97844f95d66bfc0e8bdcd7499ba8f6c15077
Robert McBride <robmcbride@gmail.com>
2011-10-21 11:07:08 -0700

    Removed all the json return stuff created by scaffolder.
    Controller is much shorter now.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,69 +1,36 @@
 class ProductsController < SecureController
-  # GET /products
   def index
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
   end
 
-  # GET /products/1
   def show
-    # I want the products list to
-    # show on this screen as well
     @products = Product.all
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @product }
-    end
   end
 
-  # GET /products/new
   def new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
   def create
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render json: @product, status: :created, location: @product }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render action: "new" 
     end
   end
 
-  # PUT /products/1
   def update
-    respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update_attributes(params[:product])
+      redirect_to @product, notice: 'Product was successfully updated.' 
+    else
+      render action: "edit" 
     end
   end
 
-  # DELETE /products/1
   def destroy
     @product.destroy
 
-    respond_to do |format|
-      format.html { redirect_to products_url }
-      format.json { head :ok }
-    end
+    redirect_to products_url 
   end
 end
